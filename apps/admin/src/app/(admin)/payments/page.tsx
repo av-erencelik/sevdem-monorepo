79dd83864fa20b1e7afab116e628b72bb6ccdaf8
av-erencelik <erenmemo05@gmail.com>
2023-05-12 09:56:13 +0300

    added table and tanstack table with data table example

---
+++ apps/admin/src/app/(admin)/payments/page.tsx
@@ -0,0 +1,85 @@
+import { Payment, columns } from "./columns";
+import { DataTable } from "./data-table";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "489e1d42",
+      amount: 125,
+      status: "processing",
+      email: "example@gmail.com",
+    },
+    {
+      id: "7282d52f",
+      amount: 150,
+      status: "failed",
+      email: "n@example.com",
+    },
+    {
+      id: "7282d52f",
+      amount: 150,
+      status: "failed",
+      email: "n@example.com",
+    },
+    {
+      id: "7282d52f",
+      amount: 150,
+      status: "failed",
+      email: "n@example.com",
+    },
+    {
+      id: "7282d52f",
+      amount: 150,
+      status: "failed",
+      email: "n@example.com",
+    },
+    {
+      id: "7282d52f",
+      amount: 150,
+      status: "failed",
+      email: "n@example.com",
+    },
+    {
+      id: "7282d52f",
+      amount: 150,
+      status: "failed",
+      email: "n@example.com",
+    },
+    {
+      id: "7282d52f",
+      amount: 150,
+      status: "failed",
+      email: "n@example.com",
+    },
+    {
+      id: "7282d52f",
+      amount: 150,
+      status: "failed",
+      email: "n@example.com",
+    },
+    {
+      id: "7282d52f",
+      amount: 150,
+      status: "pending",
+      email: "n@example.com",
+    },
+    // ...
+  ];
+}
+
+export default async function DemoPage() {
+  const data = await getData();
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -7,3 +7,7 @@ export * from "./card";
 export * from "./input";
 export * from "./scroll-area";
 export * from "./textarea";
+export * from "./table";
+export * from "./dropdown-menu";
+export * from "./badge";
+export * from "./checkbox";
