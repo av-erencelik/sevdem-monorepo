d347c907bf1baad3abec7c57251ddc69a8e6e941
av-erencelik <erenmemo05@gmail.com>
2023-05-16 23:12:09 +0300

    migrated to prisma orm and uninstalled drizzle

--- apps/admin/src/types/schemas.ts
+++ apps/admin/src/types/schemas.ts
@@ -1,18 +1,5 @@
 import { z } from "zod";
 import { createInsertSchema } from "drizzle-zod";
-import {
-  externalCost,
-  ingredient,
-  ingredientInventory,
-  measurement,
-  order,
-  orderItem,
-  price,
-  recipe,
-  recipeIngredient,
-  sale,
-  saleItem,
-} from "@/db/schema";
 
 export const signInSchema = z
   .object({
@@ -20,15 +7,3 @@ export const signInSchema = z
     password: z.string().min(6, "Şifre en az 6 karakter olmalıdır"),
   })
   .required();
-
-export const insertMeasurementSchema = createInsertSchema(measurement);
-export const insertIngredientSchema = createInsertSchema(ingredient);
-export const insertPriceSchema = createInsertSchema(price);
-export const insertRecipeSchema = createInsertSchema(recipe);
-export const insertRecipeIngredientSchema = createInsertSchema(recipeIngredient);
-export const insertIngredientInventorySchema = createInsertSchema(ingredientInventory);
-export const insertOrderSchema = createInsertSchema(order);
-export const insertOrderItemSchema = createInsertSchema(orderItem);
-export const insertSaleSchema = createInsertSchema(sale);
-export const insertSaleItemSchema = createInsertSchema(saleItem);
-export const insertExternalCostSchema = createInsertSchema(externalCost);
