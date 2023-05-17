565792c25223ba59c82fa94fcd57d0baf64dea08
av-erencelik <erenmemo05@gmail.com>
2023-05-17 08:23:43 +0300

    fixed eslint errors

--- apps/admin/src/app/(admin)/page.tsx
+++ apps/admin/src/app/(admin)/page.tsx
@@ -1,4 +1,3 @@
-export const runtime = "edge";
 export default async function Home() {
   return <div className="flex flex-col items-center justify-center gap-20 bg-cyan-50 pb-10">giris yapıldı</div>;
 }
