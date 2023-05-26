7fdd5163bc8f56c2f7646bb274232bf466838141
av-erencelik <erenmemo05@gmail.com>
2023-05-26 20:05:09 +0300

    changed primary css variable

--- apps/admin/src/app/(public)/giris/page.tsx
+++ apps/admin/src/app/(public)/giris/page.tsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
   return (
     <main className="flex min-h-[100vh] flex-col items-center justify-center gap-10 ">
       <div className="flex flex-col items-center">
-        <h1 className="mt-2 scroll-m-20 text-2xl font-semibold tracking-tight text-primary">Giriş Yap</h1>
+        <h1 className="mt-2 scroll-m-20 text-2xl font-semibold tracking-tight text-foreground">Giriş Yap</h1>
       </div>
       <LoginForm />
     </main>
