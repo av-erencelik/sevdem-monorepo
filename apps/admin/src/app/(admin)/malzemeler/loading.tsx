1d425b5ac8a250f40f81aef14febb23cb2b1cfe9
av-erencelik <erenmemo05@gmail.com>
2023-06-01 10:13:53 +0300

    loading.tsx for ingredients page

---
+++ apps/admin/src/app/(admin)/malzemeler/loading.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Separator, Skeleton } from "ui";
+
+const Loading = () => {
+  return (
+    <div className="space-y-6 rounded-lg border border-border p-10 pb-16 shadow-sm">
+      <div className="space-y-0.5">
+        <h2 className="text-2xl font-bold tracking-tight">Malzemeler</h2>
+        <p className="text-muted-foreground">Şimdiye kadar eklediğin tüm malzemeleri buradan inceleyebilirsin</p>
+      </div>
+      <Separator className="my-6" />,
+      <Skeleton className="h-[800px]" />
+    </div>
+  );
+};
+
+export default Loading;
