a7c2d21ccd7732c9c876e47374d346f6162519bf
av-erencelik <erenmemo05@gmail.com>
2023-06-04 12:14:49 +0300

    changed details page design of ingredient and updated loading.tsx according to that

--- apps/admin/src/app/(admin)/malzemeler/[ingredientId]/loading.tsx
+++ apps/admin/src/app/(admin)/malzemeler/[ingredientId]/loading.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Separator, Skeleton } from "ui";
+import { Skeleton } from "ui";
 
 const Loading = () => {
   return (
-    <div className="space-y-6 rounded-lg border border-border p-10 pb-16 shadow-sm">
-      <Skeleton className="h-[800px]" />
-    </div>
+    <>
+      <Skeleton className="mt-6 h-24 w-full p-5" />
+      <div className="mt-6 grid grid-cols-4 gap-8">
+        <Skeleton className="h-28" />
+        <Skeleton className="h-28" />
+        <Skeleton className="h-28" />
+        <Skeleton className="h-28" />
+      </div>
+      <Skeleton className="mt-6 h-96 w-full p-5" />
+      <Skeleton className="mt-6 h-96 w-full p-5" />
+    </>
   );
 };
 
